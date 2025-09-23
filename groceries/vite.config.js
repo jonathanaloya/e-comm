7c84ce647bc533a6acd0fceab6d664b3ff662b08
jonathanaloya <alo23004@byui.edu>
2025-09-23 17:03:02 +0300

    fix errors

--- groceries/vite.config.js
+++ groceries/vite.config.js
@@ -15,13 +15,7 @@ export default defineConfig({
       }
     },
     chunkSizeWarningLimit: 1000,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true
-      }
-    }
+    minify: 'esbuild'
   },
   server: {
     port: 5173
