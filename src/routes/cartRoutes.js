95413d670e764d91dfd8c0750fb2f482f596fd6e
jonathanaloya <alo23004@byui.edu>
2025-09-29 20:22:54 +0300

    cart items

--- src/routes/cartRoutes.js
+++ src/routes/cartRoutes.js
@@ -7,7 +7,7 @@ const cartRouter = Router()
 
 cartRouter.post('/create', addToCartItemController)
 cartRouter.get("/get",getCartItemController)
-cartRouter.put('/update-qty',authMiddleware, csrfProtection, updateCartItemQtyController)
-cartRouter.delete('/delete-cart-item',authMiddleware, csrfProtection, deleteCartItemQtyController)
+cartRouter.put('/update-qty', updateCartItemQtyController)
+cartRouter.delete('/delete-cart-item', deleteCartItemQtyController)
 
 export default cartRouter
