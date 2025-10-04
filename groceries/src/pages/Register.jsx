9679d14923444d299051470cc15e190af7f0a037
jonathanaloya <alo23004@byui.edu>
2025-10-04 08:01:44 +0300

    toast display fix on registration

--- groceries/src/pages/Register.jsx
+++ groceries/src/pages/Register.jsx
@@ -31,7 +31,7 @@ function Register() {
   const getPasswordErrors = (password) => {
     const errors = [];
     if (password.length < 8) {
-      errors.push("Password must be at least 8 characters.");
+      errors.push("Password must be at least 8 characters including letters, numbers, and special characters.");
     }
     if (!/[0-9]/.test(password)) {
       errors.push("Password must include at least one number.");
