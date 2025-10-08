cbdc65faeeef5422e7f90e7720c3938734e91059
jonathanaloya <alo23004@byui.edu>
2025-10-08 22:21:22 +0300

    admin

--- admin-portal/src/utils/api.js
+++ admin-portal/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import toast from 'react-hot-toast'
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://e-comm-rho-five.vercel.app/'
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || ''
 
 const api = axios.create({
   baseURL: API_BASE_URL,
