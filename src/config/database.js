bcedc9ed1ab770adaa4e03a5717111c7ae43d8c7
jonathanaloya <alo23004@byui.edu>
2025-10-30 15:01:51 +0300

    database fix

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {
+        await mongoose.connect(process.env.MONGO_URI, { 
             useNewUrlParser: true, 
             useUnifiedTopology: true,
             serverSelectionTimeoutMS: 10000,
