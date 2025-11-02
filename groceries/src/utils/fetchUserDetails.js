e34c847bab9f4457bdb5a18ef4e849ca555b766d
jonathanaloya <alo23004@byui.edu>
2025-11-02 15:13:31 +0300

    session expiry

--- groceries/src/utils/fetchUserDetails.js
+++ groceries/src/utils/fetchUserDetails.js
@@ -9,6 +9,24 @@ const fetchUserDetails = async() => {
         return response.data
     } catch (error) {
         console.log('User not authenticated:', error.response?.status)
+
+        // If session expired, clear local state immediately
+        if (error.response?.status === 401 && error.response?.data?.sessionExpired) {
+            console.log('Session expired during user details fetch - clearing local state')
+            // Import store and actions dynamically to avoid circular dependencies
+            import('../store/store').then(({ store }) => {
+                import('../store/userSlice').then(({ logout }) => {
+                    import('../store/cartProduct').then(({ handleAddItemCart }) => {
+                        import('../store/orderSlice').then(({ setOrder }) => {
+                            store.dispatch(logout())
+                            store.dispatch(handleAddItemCart([]))
+                            store.dispatch(setOrder([]))
+                        })
+                    })
+                })
+            })
+        }
+
         return { data: null, error: true }
     }
 }
