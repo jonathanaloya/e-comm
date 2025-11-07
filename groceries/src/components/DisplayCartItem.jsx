92fa835e7a03a47274639464cf3388e670967f25
jonathanaloya <alo23004@byui.edu>
2025-11-07 15:26:20 +0300

    cart card update

--- groceries/src/components/DisplayCartItem.jsx
+++ groceries/src/components/DisplayCartItem.jsx
@@ -17,16 +17,10 @@ const DisplayCartItem = ({close}) => {
     const navigate = useNavigate()
 
     const redirectToCheckoutPage = ()=>{
-        if(user._id){
-            navigate("/checkout")
-            return
-        }else{
-            navigate("/checkout")
-            if(close){
-                close()
-            }
-            return
+        if(close){
+            close()
         }
+        navigate("/checkout")
     }
   return (
     <section className='bg-neutral-900 fixed top-0 bottom-0 right-0 left-0 bg-opacity-70 z-50 flex justify-end'>
